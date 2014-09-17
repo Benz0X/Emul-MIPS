410a27686c6bdb89ac724de3d77d95a931d7b829
Benz0X <nils.exibard@gmail.com>
2014-09-17 12:58:53 +0200

    Draft Script/interactive mode

--- main.c
+++ main.c
@@ -3,8 +3,28 @@
 #include "user_int.h"
 #include "main.h"
 
-int main()
+int main(int argc, char *argv[])
 {
-    printf("Hello world!\n");
-    return 0;
+FILE *script_file = NULL;
+if (argc > 1)
+	{
+	script_file = fopen (argv[1], "r");
+	if (script_file != NULL)
+		{
+		//exécution du script while command != debug
+		printf("exécution du script\n");
+		//while (command != debug || strtok != EOF)
+		return 0;
+		}
+    	else 
+		{
+		//renvoi d'erreur
+		printf("erreur à l'ouverture du script\n");
+		exit(1);
+		}
+	}
+//exécution normale
+printf("en attente de commande utilisateur\n");
+//while (command != quit || command != resume)
+return 0;
 }
