1be7e56489f01363af691e2d5a5eb17c7bdcb87a
Khemal <aur.vasselle@gmail.com>
2014-10-21 19:12:36 +0200

    squelette execute

---
+++ include/execute.h
@@ -0,0 +1,86 @@
+#ifndef _execute_h
+#define _execute_h
+
+//ADDITION
+int ADD(instruction ins);
+int ADDI(instruction ins);
+int ADDIU(instruction ins);
+int ADDU(instruction ins);
+
+//AND
+int AND(instruction ins);
+int ANDI(instruction ins);
+
+//BRANCH
+int BEQ(instruction ins);
+int BGEZ(instruction ins);
+int BGTZ(instruction ins);
+int BLEZ(instruction ins);
+int BLTZ(instruction ins);
+int BNE(instruction ins);
+
+//BREAK
+int BREAK(instruction ins);
+
+//DIV
+int DIV(instruction ins);
+
+//JUMP
+int J(instruction ins);
+int JAL(instruction ins);
+int JALR(instruction ins);
+int JR(instruction ins);
+
+//LOAD
+int LB(instruction ins);
+int LBU(instruction ins);
+int LUI(instruction ins);
+int LW(instruction ins);
+
+//MF
+int MFHI(instruction ins);
+int MFLO(instruction ins);
+
+//MULT
+int MULT(instruction ins);
+
+//NOP
+int NOP(instruction ins);
+
+//OR
+int OR(instruction ins);
+int ORI(instruction ins);
+int XOR(instruction ins);
+
+//STORE
+int SB(instruction ins);
+int SW(instruction ins);
+
+//SIGN EXTEND
+int SEB(instruction ins);
+
+//SHIFT
+int SLL(instruction ins);
+int SRA(instruction ins);
+int SRL(instruction ins);
+
+//SET
+int SLT(instruction ins);
+int SLTI(instruction ins);
+int SLTIU(instruction ins);
+int SLTU(instruction ins);
+
+//SUB
+int SUB(instruction ins);
+int SUBU(instruction ins);
+
+//SYSCALL
+int SYSCALL(instruction ins);
+
+
+
+
+
+
+
+#endif
