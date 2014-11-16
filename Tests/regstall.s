aede6be8a49be19bc876ae547e120dc34e2faf5b
Benz0X <nils.exibard@gmail.com>
2014-11-16 20:48:30 +0100

    regstall

--- Tests/regstall.s
+++ Tests/regstall.s
@@ -4,10 +4,15 @@
 
 _start:
 .global _start
-
 addi $t1,$zero,8
+addi $t1,$zero,8	#don't need stalling
+NOP
+NOP
 addi $t1,$zero,8
-addi $t1,$zero,8
+addi $t2,$t1,8		#need stalling
+NOP
+NOP
+addi $t2,$t2,8		#don't need stalling
 
 KO:
 ADDI $v0,$zero,10  # appel explicite à exit
