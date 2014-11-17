6e4053314ca05af96b9f4886f600e136a045bf4b
Khemal <aur.vasselle@gmail.com>
2014-11-17 23:00:25 +0100

    test vsyscall

---
+++ Tests/execute/vsyscall.s
@@ -0,0 +1,27 @@
+.set noreorder
+.text
+
+li $a0, 19
+li $v0, 1	#printf 19
+syscall
+
+li $a0,  0x4000
+li $v0, 4
+syscall		#printf "youpi"
+
+li $v0, 5
+syscall		#scanf $v0 as integer
+
+li $a1, 6
+li $v0, 5
+syscall		#scanf asciiz chaine
+
+li $a0,  0x4000
+li $v0, 4
+syscall		#printf "your chain"
+
+li $v0, 10
+syscall
+
+.data 
+chaine: .asciiz "Youpi"
